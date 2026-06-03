1122cb3afafd7f08a37c350235c2f847a8152b04
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-03 15:23:11 +0530

    import the main modules and added mongodb connection. add a fix to mongodb connection error

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import { connectToMongoDB } from "./mongodb/mongodb.js";
+
+// to fix a mongodb connection error. Not sure why it happens
+import dns from "node:dns/promises"
+dns.setServers(["8.8.8.8","1.1.1.1"]) 
+
+const server = express();
+
+// Midleware
+server.use(express.json());
+server.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectToMongoDB()
+    server.listen(PORT, () => {
+      console.log(`Server is Running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error in startServer!:",error);
+    process.exit(1);
+  }
+};
+
+startServer();
