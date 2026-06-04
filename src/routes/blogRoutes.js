f8340a6da5bf9667b9c7b10705415f4abad82c40
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-04 14:21:32 +0530

    add authentication to addBlog route

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addBlog } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
+import { auth } from "../middleware/auth.js";
 
 const blogRouter = Router();
 
-blogRouter.post("/add",upload.single("image"), addBlog);
+blogRouter.post("/add", auth, upload.single("image"), addBlog);
 
 export const blogRouter;
