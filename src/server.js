5840159f46e6852e29bb05781e3366b8e2dc0d8c
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-04 13:41:42 +0530

    cloudinary integration and mongodb path correction

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-import { connectToMongoDB } from "./mongodb/mongodb.js";
+import { connectToMongoDB } from "./configs/mongodb.js";
 
 // to fix a mongodb connection error. Not sure why it happens
-import dns from "node:dns/promises"
+import dns from "node:dns/promises";
 import adminRouter from "./routes/adminRoutes.js";
-dns.setServers(["8.8.8.8","1.1.1.1"]) 
+import connectCloudinary from "./configs/cloudinary.js";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const server = express();
 
@@ -16,18 +17,19 @@ server.use(express.json());
 server.use(cors());
 
 //Routes
-server.use("/api/auth/",adminRouter)
+server.use("/api/auth/", adminRouter);
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    await connectToMongoDB()
+    connectCloudinary();
+    await connectToMongoDB();
     server.listen(PORT, () => {
       console.log(`Server is Running on PORT: ${PORT}`);
     });
   } catch (error) {
-    console.log("Error in startServer!:",error);
+    console.log("Error in startServer!:", error);
     process.exit(1);
   }
 };
