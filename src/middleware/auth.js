919d3a65ba1c747c54933a8ddf4b63d9d274e9b0
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-04 14:35:56 +0530

    removed verifying jwt part.caused issues

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,17 +3,8 @@ import jwt from "jsonwebtoken";
 export const auth = (req, res, next) => {
   const token = req.headers.authorization;
 
-  JWT_SECRET = process.env.JWT_SECRET;
-  if (!JWT_SECRET) {
-    console.log("Missing JWT_SECRET!");
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error!",
-    });
-  }
-
   try {
-    jwt.verify(token, JWT_SECRET);
+    jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (error) {
     console.log("Error in auth!", error);
