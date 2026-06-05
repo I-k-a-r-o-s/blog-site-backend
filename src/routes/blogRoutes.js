c3c6d8ef9c2eeed2db911e33437c8d78fc805484
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-05 16:04:33 +0530

    mapped the routes for the functions

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { addBlog } from "../controllers/blogController.js";
+import {
+  addBlog,
+  deleteBlog,
+  getAllBlogs,
+  getBlogById,
+  publishedState,
+} from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import { auth } from "../middleware/auth.js";
 
 const blogRouter = Router();
 
 blogRouter.post("/add", auth, upload.single("image"), addBlog);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:id", getBlogById);
+blogRouter.delete("/delete", auth, deleteBlog);
+blogRouter.post("/published-state", auth, publishedState);
 
 export default blogRouter;
