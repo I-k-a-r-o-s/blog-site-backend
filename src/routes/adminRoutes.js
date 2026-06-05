a417eec9a3b5347d9bfaed9024dd5ecc2daf25fe
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-05 17:02:20 +0530

    added routes for admin functions

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { adminLogin } from "../controllers/adminController.js";
+import {
+  adminLogin,
+  approveComment,
+  deleteComment,
+  getAllBlogsForAdmin,
+  getAllComments,
+  getDashboardContent,
+} from "../controllers/adminController.js";
+import { auth } from "../middleware/auth.js";
 
 const adminRouter = Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/blogs", auth, getAllBlogsForAdmin);
+adminRouter.delete("/delete-comment", auth, deleteComment);
+adminRouter.post("/approve-comment", auth, approveComment);
+adminRouter.get("/dashboard", auth, getDashboardContent);
 
 export default adminRouter;
